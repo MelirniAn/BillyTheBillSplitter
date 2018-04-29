d354846cf9479647a403f1fe378a399329a25367
Melirni An <melirnian@gmail.com>
2018-04-29 02:13:48 -0400

    Integrating the Parser class

--- app/src/main/java/com/example/mel/billythebillsplitter/act2.java
+++ app/src/main/java/com/example/mel/billythebillsplitter/act2.java
@@ -1,14 +1,14 @@
 package com.example.mel.billythebillsplitter;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.media.ExifInterface;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.SparseArray;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,16 +18,13 @@ import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 
 import java.io.File;
-import java.io.IOException;
 
 import edmt.dev.androidcamera2api.R;
-
-import static android.os.Environment.getExternalStorageDirectory;
 
 public class act2 extends AppCompatActivity {
 
     ImageView i1;
-    TextView t1;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,14 +32,30 @@ public class act2 extends AppCompatActivity {
         setContentView(R.layout.activity_act2);
 
         i1 = (ImageView) findViewById(R.id.imageView);
-        t1 = (TextView) findViewById(R.id.text_value);
+        textView = (TextView) findViewById(R.id.text_value);
+
 
 
 
     }
 
+    //onClickListener to move to the Picker class
+
+  //  protected StringBuilder sb = new StringBuilder();
+
+    private void configureNextButton(){
+        Button nextButton = (Button) findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(act2.this,Picker.class));
+            }
+        });
+
+    }
+
     public void getTextFromImage(View v)    {
-//        Bitmap bitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), Environment.getExternalStorageDirectory()+"/"+ "receiptPhoto" +".jpg");
+
 
         File imgFile = new File(Environment.getExternalStorageDirectory()+"/"+ "receiptPhoto" +".jpg");
 
@@ -56,36 +69,10 @@ public class act2 extends AppCompatActivity {
 
         }
 
-        //picturePath = getIntent().getStringExtra(Environment.getExternalStorageDirectory() + "/" + "receiptPhoto" + ".jpg");
-//        Bitmap myBitmap = BitmapFactory.decodeFile(Environment.getExternalStorageDirectory() + "/" + "receiptPhoto" + ".jpg");
-//        ExifInterface exif = null;
-//        try {
-//            exif = new ExifInterface(Environment.getExternalStorageDirectory() + "/" + "receiptPhoto" + ".jpg");
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
-//
-//        Matrix matrix = new Matrix();
-//        switch (orientation) {
-//            case ExifInterface.ORIENTATION_ROTATE_90:
-//                matrix.postRotate(180);
-//                break;
-//            case ExifInterface.ORIENTATION_ROTATE_180:
-//                matrix.postRotate(270);
-//                break;
-//            case ExifInterface.ORIENTATION_ROTATE_270:
-//                matrix.postRotate(90);
-//                break;
-//            default:
-//                break;
-//        }
-//
-//        i1.setImageBitmap(myBitmap);
-
 
 
         TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+        StringBuilder sb = new StringBuilder();
 
         if(!textRecognizer.isOperational()) {
             Toast.makeText(getApplicationContext(),"Could not get Text",Toast.LENGTH_SHORT).show();
@@ -95,7 +82,6 @@ public class act2 extends AppCompatActivity {
 
             SparseArray<TextBlock> items = textRecognizer.detect(frame);
 
-            StringBuilder sb = new StringBuilder();
 
             for(int i=0; i<items.size();++i)    {
                 TextBlock myItem = items.valueAt(i);
@@ -103,11 +89,23 @@ public class act2 extends AppCompatActivity {
                 sb.append("\n");
             }
 
-            t1.setText(sb.toString());
+
+            textView.setText(sb.toString());
+
+
+            configureNextButton();
         }
+
 
        // myBitmap.recycle();
     }
+
+//    public StringBuilder getResults(){
+//        return sb;
+//    }
+
+
+
 
 
 }
